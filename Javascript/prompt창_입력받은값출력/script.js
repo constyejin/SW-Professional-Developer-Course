90d306b120233f7270a3e2dc2f669b854581ff5a
constyejin <lightup.jin@gmail.com>
2023-06-08 10:36:32 +0900

    🎨 prompt창 입력받은 값 폴더 생성 (html, js)

--- Javascript/prompt창_입력받은값출력/script.js
+++ Javascript/prompt창_입력받은값출력/script.js
@@ -3,11 +3,11 @@
 
 
 // prom-btn 클릭시 alert창 띄우기
-document.getElementById('prom-btn').addEventListener('click', function (){
-  let name = prompt("당신의 이름은?");
-  document.write(name);
-  console.log(name);
-})
+// document.getElementById('prom-btn').addEventListener('click', function (){
+//   let name = prompt("당신의 이름은?");
+//   document.write(name);
+//   console.log(name);
+// })
 
 // prom-btn 클릭시 prompt 창 띄우기
 // document.getElementById('prom-btn').addEventListener('click', function (){
