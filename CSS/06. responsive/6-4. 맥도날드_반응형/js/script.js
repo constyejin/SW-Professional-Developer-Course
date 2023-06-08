3be60a2060869babdc7c5214e26443bc0f7f65e6
constyejin <lightup.jin@gmail.com>
2023-06-08 10:33:40 +0900

    ✨ prompt 창에 입력받은 값 출력 (document, console)

---
+++ CSS/06. responsive/6-4. 맥도날드_반응형/js/script.js
@@ -0,0 +1,18 @@
+// let prom = prompt('숫자를 입력하세요');
+// document.write(prom);
+
+
+// prom-btn 클릭시 alert창 띄우기
+document.getElementById('prom-btn').addEventListener('click', function (){
+  let name = prompt("당신의 이름은?");
+  document.write(name);
+  console.log(name);
+})
+
+// prom-btn 클릭시 prompt 창 띄우기
+// document.getElementById('prom-btn').addEventListener('click', function (){
+//   let prom = prompt('좋아하는 색은?');
+//   document.write(prom);
+// })
+
+
