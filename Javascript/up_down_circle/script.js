9edd50774cb929ea63b854d6b1ce87f8723abea8
constyejin <lightup.jin@gmail.com>
2023-06-12 22:21:36 +0900

    ✨ querySelectorAll & forEach

--- Javascript/up_down_circle/script.js
+++ Javascript/up_down_circle/script.js
@@ -1,4 +1,5 @@
 const circles = document.querySelectorAll('.circle');
+
 circles.forEach((circle, index) => {
   circle.style.left = `${60 * index}px`;
   circle.style.animationDelay = `${0.1 * index}s`;
