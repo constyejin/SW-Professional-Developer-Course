7029a4c3dfd57d6e4a7c0f7d95d1c340b02faf69
constyejin <lightup.jin@gmail.com>
2023-07-07 10:47:54 +0900

    ✨ script 수정

--- Javascript/01. Javascript Basic/캐러셀만들기1/캐러셀2/script.js
+++ Javascript/01. Javascript Basic/캐러셀만들기1/캐러셀2/script.js
@@ -1,27 +1,29 @@
 // 현재 index값
-let prePage = 0;
 // 현재 이미지 위치
-let preImg = 0;
+let currentSlide = 0;
+let currentImage = 0;
 let nextBtn = document.querySelector('.next');
 let prevBtn = document.querySelector('.prev');
-let slideList = document.querySelector('.slide-list').clientWidth;
+let slideList = document.querySelector('.slide-list');
+let slideWidth = slideList.clientWidth;
 
 // 딱 봐도 if문 냄새가 남
 // 코드 짤 때 영어부터 짤 생각하지 말고 한글로 최대한 구체적으로 설명부터 하고 시작해야 쉬움
 
-nextBtn.addEventListener('click', function() {
-  // 현재 사진이 1번이면 2번 사진
-  // 2번 사진이면 3번 사진
-  if (prevBtn < 3) {
-    prevBtn.removeAttribute('disabled')
-    slideList.style.transform = 'translageX(' +slideList+ 'vw)'
-  }
-});
+// nextBtn.addEventListener('click', function() {
+//   // 현재 사진이 1번이면 2번 사진
+//   // 2번 사진이면 3번 사진
+//   if (preImg < 3) {
+//     prevBtn.removeAttribute('disabled')
+//     slideList.style.transform = 'translageX(' +slideList+ 'vw)'
+//   }
+// });
 
-prevBtn.addEventListener('click', function() {
-  slideBox.style.transform = 'translateX(-' + present + '00vw)';
-  present -= 1;
-});
+
+// prevBtn.addEventListener('click', function() {
+//   slideBox.style.transform = 'translateX(-' + present + '00vw)';
+//   present -= 1;
+// });
 
 
 // prevBtn.addEventListener('click', function() {
