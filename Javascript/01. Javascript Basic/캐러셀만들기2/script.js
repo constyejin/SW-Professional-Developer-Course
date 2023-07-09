1f6145ab212c7d891f7e2047f416e9f75b032b51
constyejin <lightup.jin@gmail.com>
2023-07-09 23:45:22 +0900

    ✨ 조건문으로 active class toggle

--- Javascript/01. Javascript Basic/캐러셀만들기2/script.js
+++ Javascript/01. Javascript Basic/캐러셀만들기2/script.js
@@ -1,27 +1,27 @@
 let buttons = document.querySelectorAll('.btn-group button');
 const slideList = document.querySelector('.slide-list');
 
-// buttons.forEach(function(button, index) {
-//   button.addEventListener('click', function() {
-//     let slideList = document.querySelector('.slide-list');
-//     let translateXValue = -index * 100 + 'vw';
-//     slideList.style.transform = 'translateX(' + translateXValue + ')';
+buttons.forEach(function(button, index) {
+  button.addEventListener('click', function() {
+    let slideList = document.querySelector('.slide-list');
+    let translateXValue = -index * 100 + 'vw';
+    slideList.style.transform = 'translateX(' + translateXValue + ')';
 
-//     button.classList.add('active')
+    button.classList.add('active')
 
-//     buttons.forEach(function(otherButton) {
-//       if (otherButton !== button && otherButton.classList.contains('active')) {
-//         otherButton.classList.remove('active');
-//       }
-//     });
-//   });
-//   console.log(button, index)
-// });
+    buttons.forEach(function(otherButton) {
+      if (otherButton !== button && otherButton.classList.contains('active')) {
+        otherButton.classList.remove('active');
+      }
+    });
+  });
+  console.log(button, index)
+});
 
-buttons.forEach(function(btn, index){
-  btn.addEventListener('click', () => {
-    buttons.forEach(item => {item.classList.remove('active')})
-    slideList.style.transform = `translateX(-${index}00vw)`;
-    btn.classList.add('active');
-  })
-})
+// buttons.forEach(function(btn, index){
+//   btn.addEventListener('click', () => {
+//     buttons.forEach(item => {item.classList.remove('active')})
+//     slideList.style.transform = `translateX(-${index}00vw)`;
+//     btn.classList.add('active');
+//   })
+// })
