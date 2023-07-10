1803d1a114ad96eaa74656d6a8c9cbdda8f58ce5
constyejin <lightup.jin@gmail.com>
2023-07-10 10:23:39 +0900

    ✨ prev, next button 클릭시 slideList 이동

--- Javascript/01. Javascript Basic/캐러셀만들기1/캐러셀2/script2.js
+++ Javascript/01. Javascript Basic/캐러셀만들기1/캐러셀2/script2.js
@@ -1,21 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let currentSlide = 0;
-  const slideWrapper = document.querySelector('.slide-wrapper');
-  const slideList = document.querySelector('.slide-list');
-  const slideItems = document.querySelectorAll('.slide-item');
-  const slideWidth = slideWrapper.clientWidth;
+let currentSlide = 0;
+let slideWrapper = document.querySelector('.slide-wrapper');
+let slideList = document.querySelector('.slide-list');
+let slideItems = document.querySelectorAll('.slide-item');
+console.log(slideItems.length)
 
-  document.querySelector('.next').addEventListener('click', function() {
-    if (currentSlide < slideItems.length - 1) {
-      currentSlide++;
-      slideList.style.transform = `translateX(-${currentSlide * 100}vw)`;
-    }
-  });
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
 
-  document.querySelector('.prev').addEventListener('click', function() {
-    if (currentSlide > 0) {
-      currentSlide--;
-      slideList.style.transform = `translateX(-${currentSlide * 100}vw)`;
-    }
-  });
+
+// slideItems.forEach(function(item){
+//   nextBtn.addEventListener('click', function(){
+//     let currentSlide = Array.from(slideItems).indexOf(item);
+//     console.log(currentSlide)
+//     if (currentSlide < slideItems.length) {
+//       slideList.style.transform = `translateX(-${currentSlide * 100}vw)`;
+//     }
+//   })
+// })
+ 
+nextBtn.addEventListener('click', function() {
+  if (currentSlide < slideItems.length - 1) {
+    currentSlide++;
+    slideList.style.transform = `translateX(-${currentSlide * 100}vw)`;
+  }
 });
+
+prevBtn.addEventListener('click', function() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    slideList.style.transform = `translateX(-${currentSlide * 100}vw)`;
+  }
+});
+
