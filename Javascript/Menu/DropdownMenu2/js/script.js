6101251726f48590f4612bbce716d860dd03adbe
constyejin <lightup.jin@gmail.com>
2023-07-14 16:33:13 +0900

    📝 eq() 설명 추가

--- Javascript/Menu/DropdownMenu2/js/script.js
+++ Javascript/Menu/DropdownMenu2/js/script.js
@@ -9,7 +9,7 @@ $(".main-menu li, .submenu-wrapper").hover(function () {
 $(".submenu-list > li").hover(function () {
   // drop-down(ul)의 자식 li 4개 중 hover된 자신의 index번호를 변수 i에 저장
   let i = $(this).index();
-  // eq(i) : eq() 인덱스 번호로 요소를 찾을 때 사용
+  // eq(i) : 인덱스 번호로 요소를 찾을 때 사용 / 선택한 요소의 인덱스 번호에 해당하는 요소를 찾을 때 사용
 
   // .main-menu li 안에있는 a태그 중 호버된 자신한테 on class 추가
   // CSS에서 .main-menu li a.on 이라는 class가 있을 경우 화면 스타일링
@@ -18,3 +18,6 @@ $(".submenu-list > li").hover(function () {
   let i = $(this).index();
   $(".main-menu li").find("a").eq(i).removeClass("on");
 })
+
+
+
