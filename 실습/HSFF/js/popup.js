00b5b8043ed3e61f95aa44af64da30817d9ff5ab
constyejin <lightup.jin@gmail.com>
2023-07-16 22:07:52 +0900

    ✨ Add setTimeout

--- 실습/HSFF/js/popup.js
+++ 실습/HSFF/js/popup.js
@@ -1,7 +1,9 @@
 let closeBtn = document.querySelector('.close-btn button');
 
 closeBtn.addEventListener('click', function(){
-  document.querySelector('.popup').style.display = 'none';
+  setTimeout(function(){
+     document.querySelector('.popup').style.display = 'none';
+  }, 3000)
 })
 
 // 오늘 하루 열지 않기 클릭시 쿠키
