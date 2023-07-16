2cd51bd70bdaaae5e5944a4e141b94dfb21f7df9
constyejin <lightup.jin@gmail.com>
2023-07-16 16:01:38 +0900

    ✨ popup창 display 속성

---
+++ 실습/HSFF/js/popup.js
@@ -0,0 +1,7 @@
+let closeBtn = document.querySelector('.close-btn button');
+
+closeBtn.addEventListener('click', function(){
+  document.querySelector('.popup').style.display = 'none';
+})
+
+// 오늘 하루 열지 않기 클릭시 쿠키
