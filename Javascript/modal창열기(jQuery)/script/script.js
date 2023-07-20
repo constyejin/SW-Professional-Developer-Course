d94142645b965c30032590b6a4fdaa01a087ea3b
constyejin <lightup.jin@gmail.com>
2023-07-20 11:17:26 +0900

    ✨ 수정된 id, class에 맞춰 코드 수정

--- Javascript/modal창열기(jQuery)/script/script.js
+++ Javascript/modal창열기(jQuery)/script/script.js
@@ -1,30 +1,38 @@
-// attr 속성값을 가져오거나 추가하는 메서드
-// #img-modal Code
-$(".img-list img").click(function () {
-  let 이미지경로 = $(this).attr("src");
-  let 대체텍스트 = $(this).attr("alt");
-  $(".img-modal-box img").attr({
-    src: 이미지경로,
-    alt: 대체텍스트
-  });
-  $(".img-modal").fadeIn();
-});
-
-$(".img-modal-btn").click(function () {
-  $(".img-modal").fadeOut();
-});
-
 // .modal Code
 $(".open-btn").click(function () {
-  $(".modal").fadeIn();
+  $(".modal-box").fadeIn();
   // 애니메이션 효과를 만드는 메소드
-  // 제이쿼리 animate 
-  $(".modal-box").animate({opacity: 1});
+  $(".overlay").fadeIn();
 });
 
 $(".modal-close").click(function () {
-  $(".modal-box").animate({opacity: 0});
-  $(".modal").fadeOut();
+  $(".modal-box").fadeOut();
+  $(".overlay").fadeOut();
 });
+
+
+// attr 속성값을 가져오거나 추가하는 메서드
+// #img-modal Code
+$(".modal-list img").click(function () {
+  let 이미지경로 = $(this).attr("src");
+  let 대체텍스트 = $(this).attr("alt");
+
+  $(".img-modal-wrapper img").attr({
+    src: 이미지경로,
+    alt: 대체텍스트
+  });
+
+  $(".img-modal-box").fadeIn();
+  $('.overlay').fadeIn();
+});
+
+
+$(".img-modal-btn").click(function () {
+  $(".img-modal-box").fadeOut();
+  $('.overlay').fadeOut();
+});
+
+
+
 
 
