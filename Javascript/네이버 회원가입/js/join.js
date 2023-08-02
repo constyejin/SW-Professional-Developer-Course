f5aa05205f70f59f394d1c7afb8db6b603e0c983
constyejin <lightup.jin@gmail.com>
2023-08-02 22:41:16 +0900

    ✨  Vanilla JS로 비밀번호 조건문 구현

--- Javascript/네이버 회원가입/js/join.js
+++ Javascript/네이버 회원가입/js/join.js
@@ -1,12 +1,13 @@
-let inputAll = document.querySelectorAll('input');
+// Essential Infomation
+let essenInfo = '<span class="text-red">필수 정보입니다.</span>';
 
-inputAll.forEach(function (input) {
+document.querySelectorAll('input').forEach(function (input) {
   input.addEventListener("focus", function () {
     this.parentNode.classList.add("input-act");
   });
 });
 
-inputAll.forEach(function (input) {
+document.querySelectorAll('input').forEach(function (input) {
   input.addEventListener("focusout", function () {
     this.parentNode.classList.remove("input-act");
   });
@@ -24,7 +25,7 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
   let idWarn = document.querySelector('.userid .warn');
 
   if(userId.length == 0) {
-    idWarn.innerHTML = '<span class="text-red">필수 정보입니다.</span>';
+    idWarn.innerHTML = essenInfo;
   } else if(!idExp.test(userId)) {
     idWarn.innerHTML = '<span class="text-red">5~8자의 영문 소문자만 사용가능합니다.</span>';
   } else {
@@ -34,4 +35,31 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
 })
 
 
-// 
+// 비밀번호
+document.querySelector('.userpw input').addEventListener('focusout', function(){
+  let userPw = this.value;
+  let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
+
+  let pwWarn = document.querySelector('.userpw .warn');
+  let pwText = document.querySelector('.userpw .inputbox p');
+  let pwImg = document.querySelector('.userpw .inputbox img');
+
+  if(userPw.length == 0) {
+    pwWarn.innerHTML = essenInfo;
+    pwText.querySelector('span').innerHTML = '';
+    pwImg.src = 'images/m_icon_pw_step_01.png';
+  } else if(!pwExp.test(userPw)) {
+    pwWarn.innerHTML = '<span class="text-red">8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.</span>';
+    pwText.innerHTML = '<span class="text-red">사용불가</span>';
+    pwImg.src = 'images/m_icon_pw_step_10.png';
+  } else {
+    pwveri = true;
+    pwWarn.innerHTML = '';
+    pwText.innerHTML = '<span class="text-green">안전</span>';
+    pwImg.src = 'images/m_icon_pw_step_04.png';
+  }
+})
+
+
+
+
