ddd8420677a35522e2a4dc1eed8acc4c51ce3a5b
constyejin <lightup.jin@gmail.com>
2023-08-16 00:35:17 +0900

    💡 각 버튼에 이중 forEach으로 click event 주석 설명 추가

--- Javascript/디지털 시계/js/script.js
+++ Javascript/디지털 시계/js/script.js
@@ -1,3 +1,6 @@
+let now = new Date();
+let hr = now.getHours();
+
 // 두자리수로 시간 표시
 setInterval(function(){
   let now = new Date();
@@ -39,44 +42,51 @@ setInterval(function(){
 }, 1000)
 
 
-let now = new Date();
-let hr = now.getHours();
-let buttons = document.querySelectorAll(".btn-list li");
-
+// 시간에 맞춰 배경 이미지랑 버튼 활성화
 // 6시 ~ 11시 까지 morning
 // 12 ~ 15시 까지 afternoon
 // 16 ~ 19시 까지 evening
 // 20 ~ night
+let btnList = document.querySelectorAll(".btn-list li");
+
 if(hr >= 6 && hr < 12) {
   document.getElementById("wrapper").classList.add("morning");
-  buttons[0].classList.add("on");
+  btnList[0].classList.add("on");
 } else if(hr >= 12 && hr < 16) {
   document.getElementById("wrapper").classList.add("afternoon");
-  buttons[1].classList.add("on");
+  btnList[1].classList.add("on");
 } else if(hr >= 16 && hr < 20) {
   document.getElementById("wrapper").classList.add("evening");
-  buttons[2].classList.add("on");
+  btnList[2].classList.add("on");
 } else {
   document.getElementById("wrapper").classList.add("night");
-  buttons[3].classList.add("on");
+  btnList[3].classList.add("on");
 }
 
 
-
-// querySelectorAll을 이용해서 nav li 들을 담아준다
-let btnList = document.querySelectorAll(".btn-list li");
+// 버튼 클릭 했을 때 배경 이미지 변경 및 on class add & remove
 let back = document.getElementById("wrapper");
 
 btnList.forEach(function(btn){
+  // 각 btn의 html 내용을 text 라는 변수에 저장
   let text = btn.querySelector("a").innerHTML;
   console.log(text);
 
-  btn.addEventListener('click', function(a){
-    a.preventDefault();
+  // 각 버튼에 클릭 이벤트 추가
+  btn.addEventListener('click', function(e){
+    // a 링크 페이지 이동하는 것 막기
+    e.preventDefault();
+
+    // 모든 버튼을 다시 한 번 돌면서 on class remove
     btnList.forEach(function(e){
       e.classList.remove('on');
     })
+    
+    // 현재 클릭된 버튼 요소에만 on class add
     btn.classList.add('on');
+    // Element.className  = 'class name';
+    // 기존에 클래스가 없으면 add, 있으면 기존 것을 우측 class name으로 변경
+    // classList.add : 해당 요소에 새로운 class add
     back.className = text;
   })
 })
